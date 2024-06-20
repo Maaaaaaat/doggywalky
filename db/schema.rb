a77f58ae4e0e199851464667ad85dde54ac19b71
Kathy Yao <kathy.yao.grison@gmail.com>
2024-06-20 11:39:40 +0200

    to crash

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2024_06_19_101847) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,7 +35,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_19_101847) do
     t.float "latitude"
     t.float "longitude"
     t.string "street"
-
   end
 
   create_table "profile_groups", force: :cascade do |t|
