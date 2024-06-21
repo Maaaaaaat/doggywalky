b7985927169b3dfdd4ddab0c9609cb139a364cec
Kathy Yao <kathy.yao.grison@gmail.com>
2024-06-21 11:02:23 +0200

    Travail en cours

--- app/controllers/walks_controller.rb
+++ app/controllers/walks_controller.rb
@@ -1,18 +1,31 @@
 class WalksController < ApplicationController
   def index
-    @group = Group.find(params[:id])
-    
   end
 
   def show
   end
 
   def new
+    @group = Group.find(params[:group_id])
+    @walk = Walk.new
   end
 
   def create
+    @walk = Walk.new(walk_params)
+    if @walk.save
+      redirect_to @walk, notice: 'Walk was successfully created.'
+    else
+      render :new
+    end
+
   end
 
   def update
+  end
+
+  private
+
+  def walk_params
+    params.require(:walk).permit(:date, :start_time, :adress)
   end
 end
