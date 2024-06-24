9a6383f328ea8b68d11fcad35b85b9cd6279f53e
mathieu blanchon <mathieu.b86@gmail.com>
2024-06-24 15:31:11 +0200

    dropdown menu qui s'affcihe tout le temps

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -35,3 +35,10 @@ searchJS.onload = function () {
     geocoder.mapboxgl = mapboxgl;
     map.addControl(geocoder);
   }
+
+  document.addEventListener('turbo:load', () => {
+    var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+    var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+      return new bootstrap.Dropdown(dropdownToggleEl)
+    });
+  });
