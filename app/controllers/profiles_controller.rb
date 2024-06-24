32bb1bbafe5838cad4bd75bf9a836a337004d00a
Luiza Nobrega <mmluiza_@hotmail.com>
2024-06-24 15:42:53 +0200

    edit profil ok

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,13 +6,13 @@ class ProfilesController < ApplicationController
   end
 
   def new
-    @profile = Profile.new
+    @profilenew = Profile.new
   end
 
   def create
-    @profile = Profile.new(profile_params)
-    @profile.user = current_user
-    if @profile.save!
+    @profilenew = Profile.new(profile_params)
+    @profilenew.user = current_user
+    if @profilenew.save!
     redirect_to root_path
     else
       render :new
