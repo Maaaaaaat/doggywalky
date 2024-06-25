ad19adc19f91a0db2e83e3cd9236c60837646a71
Kathy Yao <kathy.yao.grison@gmail.com>
2024-06-25 12:34:08 +0200

    Map a jour et qui fonctionne, bouton rejoindre et quitter un groupe, bouton rejoindre une ballade.

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -12,33 +12,3 @@ export { application }
 import "controllers";
 
 
-
-
-const ACCESS_TOKEN = 'pk.eyJ1IjoidHVjYXNzZXNsZXNjb3VpbGxlcyIsImEiOiJjbHhraG51NzEwMWd0MmpzZG02aXQ4bjlkIn0.Rf-MR-RK_4Qi75WYkOINjg';
-
-mapboxgl.accessToken = ACCESS_TOKEN;
-const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v12',
-    center: [-73.99209, 40.68933],
-    zoom: 12
-});
-
-const searchJS = document.getElementById('search-js');
-searchJS.onload = function () {
-    const geocoder = new MapboxGeocoder();
-    geocoder.accessToken = ACCESS_TOKEN;
-    geocoder.options = {
-        proximity: [-73.99209, 40.68933]
-    };
-    geocoder.marker = true;
-    geocoder.mapboxgl = mapboxgl;
-    map.addControl(geocoder);
-  }
-
-  document.addEventListener('turbo:load', () => {
-    var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
-    var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
-      return new bootstrap.Dropdown(dropdownToggleEl)
-    });
-  });
