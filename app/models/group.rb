ea8a9d6c346c052cdd25d6ef7e9d6be83c5a9de4
mathieu blanchon <mathieu.b86@gmail.com>
2024-06-26 21:30:10 +0200

    dfd

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,6 +3,7 @@ class Group < ApplicationRecord
   has_many :walks
   has_many :profile_groups
   has_many :profiles, through: :profile_groups
+  has_one :chatroom
 
   geocoded_by :city
   after_validation :geocode, if: :will_save_change_to_city?
